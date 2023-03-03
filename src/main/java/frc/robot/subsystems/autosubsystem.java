1c9901cf6e024fd42fb86138122c23d4a65403bf
FRCTEAM1474 <frcteam1474@gmail.com>
2023-03-02 20:45:29 -0500

    cont.

--- src/main/java/frc/robot/subsystems/autosubsystem.java
+++ src/main/java/frc/robot/subsystems/autosubsystem.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+import frc.robot.subsystems.drivetrainsubsystem;
 import frc.robot.Constants;
 
 public class autosubsystem extends SubsystemBase {
@@ -30,15 +31,21 @@ public class autosubsystem extends SubsystemBase {
     //converts meters into ticks to travel
     // TODO: remember to factor in the 3:1 ratio for encoder.
     //wheels spin 3 times for every one rotation of the encoder shaft.
-    bothsidestickstotravel = 
+    //4.5 meters backwards
+    bothsidestickstotravel = ((meterstotravel / (Math.PI * 6) * (4096/3)));
     return bothsidestickstotravel;
     
 }
 public static double[] turn(double degreestoturn){
+    //21.375 inches track width
+    //each side travels 0.426 meters for 180 degrees
      
-     double[] leftandrighttickstotravel = {lefttickstotravel, righttickstotravel}
+     double[] leftandrighttickstotravel = {lefttickstotravel, righttickstotravel};
 
     return leftandrighttickstotravel;
+}
+public static void turn180degrees() {
+
 }
 
     
